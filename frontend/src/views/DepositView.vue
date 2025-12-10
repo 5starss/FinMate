ac85a9df2468129b900887963abb993d75b42fb3
chowh0823 <ccc2900@naver.com>
2025-12-10 22:30:14 +0900

    프론트엔드 세팅

---
+++ frontend/src/views/DepositView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>예금</h1>
+    </div>
+    <DepositList />
+</template>
+
+<script setup>
+  import { onMounted } from 'vue'
+  import { useDepositStore } from '@/stores/deposits'
+  import DepositList from '@/components/DepositList.vue'
+
+  const store = useDepositStore()
+
+  onMounted(() => {
+    store.getDeposits()
+  })
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/SavingView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h1>적금</h1>
+    </div>
+    <SavingList />
+</template>
+
+<script setup>
+  import { onMounted } from 'vue'
+  import { useSavingStore } from '@/stores/savings'
+  import SavingList from '@/components/SavingList.vue'
+
+  const store = useSavingStore()
+
+  onMounted(() => {
+    store.getSavings()
+  })
+</script>
+
+<style>
+
+</style>
