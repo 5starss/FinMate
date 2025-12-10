5c3e224cbc5927512e263735830455cf371493b6
chowh0823 <ccc2900@naver.com>
2025-12-10 16:20:19 +0900

    기본 장고 세팅
    기본 장고 세팅 + 금융상품(예금, 적금) API 호출 및 DB 저장

---
+++ products/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.conf import settings
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests, json
+from .models import DepositProducts, DepositOptions, SavingProducts, SavingOptions
+from .selializers import DepositProductsSerializer, DepositOptionsSerializer, SavingProductsSerializer, SavingOptionsSerializer
+
+
+API_KEY = settings.API_KEY
+
+@api_view(['GET', 'POST'])
+def deposit_list(request):
+    if request.method == 'POST':
+        url = 'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json'
+        params = {
+            'auth': API_KEY,
+            'topFinGrpNo': '020000',
+            'pageNo': '1'
+        }
+        response = requests.get(url, params=params)
+        products_data = response.json()['result']['baseList']
+        options_data = response.json()['result']['optionList']
+
+        for data in products_data:
+            try:
+                tester_name = DepositProducts.objects.get(fin_prdt_cd=data['fin_prdt_cd'])
+                continue
+
+            except:
+                DepositProducts(
+                    fin_prdt_cd = data['fin_prdt_cd'],
+                    dcls_month = data['dcls_month'],
+                    kor_co_nm = data['kor_co_nm'],
+                    fin_prdt_nm = data['fin_prdt_nm'],
+                    etc_note = data['etc_note'],
+                    join_deny = data['join_deny'],
+                    join_member = data['join_member'],
+                    join_way = data['join_way'],
+                    spcl_cnd = data['spcl_cnd'],
+                ).save()
+
+        DepositOptions.objects.all().delete()
+        for data in options_data:
+            fin_prdt_cd = data['fin_prdt_cd']
+            product_obj = DepositProducts.objects.get(fin_prdt_cd=fin_prdt_cd)
+            DepositOptions(
+                intr_rate_type_nm = data['intr_rate_type_nm'],
+                intr_rate = data['intr_rate'],
+                intr_rate2 = data['intr_rate2'],
+                save_trm = data['save_trm'],
+                fin_prdt_cd = product_obj,
+            ).save()
+
+        return Response(
+            {"message": "Okay"},
+            status=status.HTTP_200_OK
+        )
+    elif request.method == 'GET':
+        deposit = get_list_or_404(DepositProducts)
+        serializer = DepositProductsSerializer(deposit, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def saving_list(request):
+    if request.method == 'POST':
+        url = 'http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json'
+        params = {
+            'auth': API_KEY,
+            'topFinGrpNo': '020000',
+            'pageNo': '1'
+        }
+        response = requests.get(url, params=params)
+        products_data = response.json()['result']['baseList']
+        options_data = response.json()['result']['optionList']
+
+        for data in products_data:
+            try:
+                tester_name = SavingProducts.objects.get(fin_prdt_cd=data['fin_prdt_cd'])
+                continue
+
+            except:
+                SavingProducts(
+                    fin_prdt_cd = data['fin_prdt_cd'],
+                    dcls_month = data['dcls_month'],
+                    kor_co_nm = data['kor_co_nm'],
+                    fin_prdt_nm = data['fin_prdt_nm'],
+                    etc_note = data['etc_note'],
+                    join_deny = data['join_deny'],
+                    join_member = data['join_member'],
+                    join_way = data['join_way'],
+                    spcl_cnd = data['spcl_cnd'],
+                ).save()
+
+        SavingOptions.objects.all().delete()
+        for data in options_data:
+            fin_prdt_cd = data['fin_prdt_cd']
+            product_obj = SavingProducts.objects.get(fin_prdt_cd=fin_prdt_cd)
+            SavingOptions(
+                intr_rate_type_nm = data['intr_rate_type_nm'],
+                intr_rate = data['intr_rate'],
+                intr_rate2 = data['intr_rate2'],
+                save_trm = data['save_trm'],
+                fin_prdt_cd = product_obj,
+            ).save()
+
+        return Response(
+            {"message": "Okay"},
+            status=status.HTTP_200_OK
+        )
+    elif request.method == 'GET':
+        saving = get_list_or_404(SavingProducts)
+        serializer = SavingProductsSerializer(saving, many=True)
+        return Response(serializer.data)
